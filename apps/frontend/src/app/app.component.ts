6f3e920abf65eab76d96f98dd052b0e2ed7479cb
Setsudashi <cryptiareta@gmail.com>
2025-01-30 21:17:42 +0100

    removed dark theme

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'SPMS';
